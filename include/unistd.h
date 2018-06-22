a5886d8e48195475f042d8032ae9d4c956e6cd8d
Thorsten Otto <admin@tho-otto.de>
2018-06-22 23:02:25 +0200

    Fix prototype of read()

--- include/unistd.h
+++ include/unistd.h
@@ -1,3 +1,9 @@
 #include <stdlib.h>
+#include <sys/types.h>
 
 int isatty (int handle);
+extern ssize_t read (int __fd, void *__buf, size_t __nbytes);
+extern ssize_t write (int __fd, __const void *__buf, size_t __n);
+extern int open(const char *filename, int access, ...);
+extern int close(int fd);
+extern int unlink(const char *filename);
