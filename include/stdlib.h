0b19acd9e0ac27db6a7a0992c0758c80a497fb66
Thorsten Otto <admin@tho-otto.de>
2019-07-21 19:50:39 +0200

    Added function srand()

--- include/stdlib.h
+++ include/stdlib.h
@@ -35,6 +35,7 @@ extern void abort(void);
 #define	EXIT_FAILURE	1	/* Failing exit status.  */
 #define	EXIT_SUCCESS	0	/* Successful exit status.  */
 
+extern void srand (unsigned int __seed);
 extern int rand(void);
 extern long lrand(void);
 extern void srand48(long int seed);
