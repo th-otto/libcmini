e01df860ef6639b58ecb064ca17431bfb6b6ecf5
Thorsten Otto <admin@tho-otto.de>
2019-07-21 16:53:21 +0200

    Replace strcmp() by version from mintlib

--- sources/strcmp.c
+++ sources/strcmp.c
@@ -7,16 +7,31 @@
 
 #include <string.h>
 
-int strcmp(const char *s1, const char *s2)
+int strcmp(const char *scan1, const char *scan2)
 {
-	int cmp;
+	register unsigned char c1, c2;
 
-	for (; *s1++ && *s2++;)
-	{
-		cmp = (*s1 - *s2);
-		if (cmp != 0) return cmp;
-	}
-	return cmp;
+	if (!scan1)
+		return scan2 ? -1 : 0;
+	if (!scan2) return 1;
+
+	do {
+		c1 = (unsigned char) *scan1++; c2 = (unsigned char) *scan2++;
+	} while (c1 && c1 == c2);
+
+	/*
+	 * The following case analysis is necessary so that characters
+	 * which look negative collate low against normal characters but
+	 * high against the end-of-string NUL.
+	 */
+	if (c1 == c2)
+		return(0);
+	else if (c1 == '\0')
+		return(-1);
+	else if (c2 == '\0')
+		return(1);
+	else
+		return(c1 - c2);
 }
 
 
