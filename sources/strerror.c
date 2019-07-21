a1b07e7d7dba75804e4080e0eb8bddb618fe2b79
Thorsten Otto <admin@tho-otto.de>
2019-07-21 23:01:46 +0200

    No need to use snprintf in strerror(); it cannot overflow

--- sources/strerror.c
+++ sources/strerror.c
@@ -12,6 +12,6 @@ static char unknown_error[] = "Unknown error 0123456789";
 
 char *strerror(int errnum) {
 
-	snprintf (unknown_error, sizeof(unknown_error),"Unknown error %u", errnum);
+	sprintf (unknown_error, "Unknown error %u", errnum);
 	return unknown_error;
 }
