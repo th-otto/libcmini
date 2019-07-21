ccfd74ca14dea44731ac257ab2a7dd96c0231634
Thorsten Otto <admin@tho-otto.de>
2019-07-21 22:55:34 +0200

    Fix ltoa

--- sources/ltoa.c
+++ sources/ltoa.c
@@ -1,18 +1,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-static char _numstr[] = "0123456789ABCDEF";
+static char const _numstr[] = "0123456789ABCDEF";
 
 char *ltoa(long value, char *buffer, int radix)
 {
 	char *p = buffer;
+	int neg = 0;
 
+	if (value < 0)
+	{
+		neg = 1;
+	}
 	do {
 		*p++ = _numstr[value % radix];
-	} while ((value /= radix) > 0);
+	} while ((value /= radix) != 0);
 
+	if (neg)
+		*p++ = '-';
 	*p = '\0';
 
 	return strrev(buffer);
 }
-
