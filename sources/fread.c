aa3df42cdb6541ba53fc110293e80768910f0fc6
Thorsten Otto <admin@tho-otto.de>
2019-07-22 13:01:58 +0200

    Fix fread()/fwrite()

--- sources/fread.c
+++ sources/fread.c
@@ -5,6 +5,5 @@
 size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream)
 {
 	long ret = Fread(FILE_GET_HANDLE(stream), size * nmemb, ptr);
-	return ret<0 ? 0 : ret;
+	return ret < 0 ? 0 : ret / size;
 }
-
