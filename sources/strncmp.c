c91d82517aa9ca4e5af6c3c179fd596162b58326
Thorsten Otto <admin@tho-otto.de>
2019-07-24 03:58:31 +0200

    Replace strncmp by mintlib version

--- sources/strncmp.c
+++ sources/strncmp.c
@@ -8,15 +8,35 @@
 #include <string.h>
 #include <sys/types.h>
 
-int strncmp(const char *s1, const char *s2, size_t max)
+int strncmp(const char *scan1, const char *scan2, size_t n)
 {
-	register ssize_t count = max;
 	register unsigned char c1, c2;
-	
+	register long count;  /* FIXME!!! Has to be size_t but that requires
+				a rewrite (Guido).  */
+
+	if (!scan1) {
+		return scan2 ? -1 : 0;
+	}
+	if (!scan2) return 1;
+	count = n;
 	do {
-		c1 = (unsigned char) *s1++; c2 = (unsigned char) *s2++;
+		c1 = (unsigned char) *scan1++; c2 = (unsigned char) *scan2++;
 	} while (--count >= 0 && c1 && c1 == c2);
+
 	if (count < 0)
 		return(0);
-	return c1 - c2;
+
+	/*
+	 * The following case analysis is necessary so that characters
+	 * which look negative collate low against normal characters but
+	 * high against the end-of-string NUL.
+	 */
+	if (c1 == c2)
+		return(0);
+	else if (c1 == '\0')
+		return(-1);
+	else if (c2 == '\0')
+		return(1);
+	else
+		return(c1 - c2);
 }
