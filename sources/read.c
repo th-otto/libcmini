671bc68a6ab155de345a7d90723f0ba3ca033caf
Thorsten Otto <admin@tho-otto.de>
2019-07-24 11:56:04 +0200

    Fix setting errno in read()

--- sources/read.c
+++ sources/read.c
@@ -11,7 +11,7 @@ ssize_t read(int fd, void *buf, size_t nbytes)
 
     if (result < 0)
     {
-        __set_errno(result);
+        __set_errno(-result);
         return -1;
     }
     return result;
