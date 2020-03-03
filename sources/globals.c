e5c128e2e9318f3adfeac25f9139e027313feb53
Thorsten Otto <admin@tho-otto.de>
2020-03-03 03:57:01 +0100

    Move stdio globals to stdio.c

--- sources/globals.c
+++ sources/globals.c
@@ -2,6 +2,12 @@
 
 /* globals */
 
+#ifdef __GNUC__
+
+/*
+ * For Pure-C, these variables are defined in in the startup module
+ */
+
 short _app = 1;
 unsigned long _PgmSize;
 BASEPAGE *_base=0;
@@ -16,10 +22,4 @@ long __libc_argc = 1;
 static char *__libc_argv_default[] = { "unknown application", NULL };
 char **__libc_argv = __libc_argv_default;
 
-static FILE _StdInF =  { _IOMAGIC, (void *)0, 0, NULL, 0, 0, 0 };
-static FILE _StdOutF = { _IOMAGIC, (void *)1, 0, NULL, 0, 0, 0 };
-static FILE _StdErrF = { _IOMAGIC, (void *)2, 0, NULL, 0, 0, 0 };
-
-FILE *stdin = &_StdInF;
-FILE *stdout = &_StdOutF;
-FILE *stderr = &_StdErrF;
+#endif
